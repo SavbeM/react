6172cc0f246b366284e0aa4f721092785b9001c4
SavbeM <mark.selihov2458@gmail.com>
2024-04-26 20:55:11 +0300

    first task

--- src/item-container/product-list/product-filter/ProductFilter.js
+++ src/item-container/product-list/product-filter/ProductFilter.js
@@ -1,51 +1,70 @@
 import './ProductFilter.css';
+import {useState} from "react";
 
-function ProductFilter(){
- return(  
-    <>
+function ProductFilter() {
+    const [isActive, setIsActive] = useState([false, false, false, false, false]);
 
-        <div className="filter-container">
-            <input 
-                type="radio" 
-                name="filter" 
-                value="all"
-            />
-            <span>Všetko</span>
-            
-            <input 
-                type="radio" 
-                name="filter" 
-                value="men"
-            />
-            <span>Pánska obuv</span>
+    const buttonClass = {
+        default: "custom-button",
+        active: "active-button"
+    }
 
-            <input 
-                type="radio" 
-                name="filter" 
-                value="women" 
-            />
-            <span>Dámska obuv</span>
-
-            <input 
-                type="radio" 
-                name="filter" 
-                value="kids" 
-            />
-        <span>Detská obuv</span>
-        </div>
-
-        <div className="category-container">
-            <button className="custom-button">Tenisky</button>
-            <button className="custom-button">Komfortná obuv</button>
-            <button className="custom-button">Formálna obuv</button>
-            <button className="custom-button">Obuv na doma</button>
-            <button className="custom-button">Outdoorová obuv</button>
-        </div>
-
-    </> 
-);
+    const [buttonStyle, setButtonStyle] = useState([buttonClass.default, buttonClass.default, buttonClass.default, buttonClass.default, buttonClass.default]);
 
 
+    const genderCategory = [
+        {name: "Všetko", value: "all"},
+        {name: "Pánska obuv", value: "men"},
+        {name: "Dámska obuv", value: "women"},
+        {name: "Detská obuv", value: "kids"}
+    ]
+
+    const typeCategory = [
+        {name: "Tenisky"},
+        {name: "Komfortná obuv"},
+        {name: "Formálna obuv"},
+        {name: "Obuv na doma"},
+        {name: "Outdoorová obuv"}
+    ]
+
+    const handleButton = (index) => {
+        setIsActive(prevState => [...prevState, prevState[index] = !prevState[index]]);
+        setButtonStyle(prevState => [...prevState, prevState[index] = isActive[index] ? buttonClass.active : buttonClass.default]);
+        console.log(isActive)
+        console.log(buttonStyle)
+    }
+
+
+
+    return (
+        <>
+            <div className="filter-container">
+                {genderCategory.map((category, index) => {
+                    return (
+                        <div className="filter-item" key={index}>
+                            <p>{category.name}</p>
+                            <input
+                                type="radio"
+                                name="filter"
+                                value={category.value}
+                            />
+
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className="category-container">
+                {typeCategory.map((category, index) => {
+                    return (
+                        <button key={index} className={buttonStyle[index]}
+                                onClick={() => handleButton(index)}>{category.name}</button>
+                    )
+                })}
+            </div>
+
+        </>
+    );
 }
 
 export default ProductFilter;
